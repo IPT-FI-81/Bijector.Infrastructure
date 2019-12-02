ee4e7126cc6ac26fe2bc30b3f1f01b4e26d90106
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2019-12-02 17:13:53 +0200

    Add context to dispatcher methods

--- src/Dispatchers/ICommandDispatcher.cs
+++ src/Dispatchers/ICommandDispatcher.cs
@@ -1,10 +1,11 @@
 using System.Threading.Tasks;
+using Bijector.Infrastructure.Types;
 using Bijector.Infrastructure.Types.Messages;
 
 namespace Bijector.Infrastructure.Dispatchers
 {
     public interface ICommandDispatcher
     {
-        Task SendAsync<TCommand>(TCommand command) where TCommand : ICommand;
+        Task SendAsync<TCommand>(TCommand command, IContext context) where TCommand : ICommand;
     }
 }
--- src/Dispatchers/IQueryDispatcher.cs
+++ src/Dispatchers/IQueryDispatcher.cs
@@ -1,10 +1,11 @@
 using System.Threading.Tasks;
+using Bijector.Infrastructure.Types;
 using Bijector.Infrastructure.Types.Messages;
 
 namespace Bijector.Infrastructure.Dispatchers
 {
     public interface IQueryDispatcher
     {
-        Task<TResponse> QueryAsync<TResponse>(IQuery<TResponse> command);
+        Task<TResponse> QueryAsync<TResponse>(IQuery<TResponse> query, IContext context);
     }
 }
